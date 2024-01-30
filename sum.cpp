0bc754495f3554b5602f981e978404de543f132f
Sakshimehra072 <sakshimehra072@gmail.com>
2024-01-30 14:15:54 +0530

    added sum file

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter fist number : ";
+    cin >> a;
+    cout << "enter second number :";
+    cin >> b;
+    cout << "sum is :";
+    cout << a + b;
+    return 0;
+};
