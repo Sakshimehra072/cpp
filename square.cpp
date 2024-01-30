be122c589f6add06a8d614ca67e2c8ddcd415113
Sakshimehra072 <sakshimehra072@gmail.com>
2024-01-30 14:18:50 +0530

    added square file

---
+++ square.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a;
+//     cout << "enter a number : ";
+//     cin >> a;
+//     cout << "square of number is :";
+//     cout << a * a;
+//     return 0;
+//}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter first number  : ";
+    cin >> a;
+    cout << "and second number : ";
+    cin >> b;
+    cout << a + b;
+    return 0;
+}
